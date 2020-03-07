f5dc452391297e6465fcc7f695f2ca382b1b011c
Junsong Fan <junsongfc76@gmail.com>
2020-03-07 18:18:10 +0800

    Update models

--- lib/models/__init__.py
+++ lib/models/__init__.py
@@ -1,4 +1,5 @@
 from .layers_custom import *
 from .cian_models import MultiScale
 from .cian_models import resnet101_largefov_SA, resnet101_largefov_CA
+from .cian_models import resnet50_largefov_SA, resnet50_largefov_CA
 
